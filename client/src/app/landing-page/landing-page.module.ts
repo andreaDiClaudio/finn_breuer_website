b3a9d05ce14e991f1857c3b47ad6508b80ec2d69
andreaDiClaudio <andreadicla@gmail.com>
2023-11-16 17:08:34 +0100

    created navbar with animation

--- client/src/app/landing-page/landing-page.module.ts
+++ client/src/app/landing-page/landing-page.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page.component';
+import {NavbarModule} from "../navbar/navbar.module";
 
 
 @NgModule({
   declarations: [
     LandingPageComponent
   ],
-  imports: [
-    CommonModule,
-    LandingPageRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        LandingPageRoutingModule,
+        NavbarModule
+    ]
 })
 export class LandingPageModule { }
