df463d0b8f863b5025751725f17f2dfdc61e14b2
andreaDiClaudio <andreadicla@gmail.com>
2023-11-16 15:53:31 +0100

    created closed burger menu

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./landing-page/landing-page.module').then(m => m.LandingPageModule) }
+  { path: '', loadChildren: () => import('./landing-page/landing-page.module').then(m => m.LandingPageModule) },
+  { path: 'navbar', loadChildren: () => import('./navbar/navbar.module').then(m => m.NavbarModule) }
 ];
 
 @NgModule({
