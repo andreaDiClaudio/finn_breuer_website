0a5756a817cdf6c1be38a3f2e63c3456db14d34c
andreaDiClaudio <andreadicla@gmail.com>
2023-11-20 18:52:01 +0100

    implemented show reel works section

--- client/src/app/landing-page/landing-page.module.ts
+++ client/src/app/landing-page/landing-page.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page.component';
 import {NavbarModule} from "../navbar/navbar.module";
+import { ShowreelWorksComponent } from './showreel-works/showreel-works.component';
 
 
 @NgModule({
   declarations: [
-    LandingPageComponent
+    LandingPageComponent,
+    ShowreelWorksComponent
   ],
     imports: [
         CommonModule,
---
+++ client/src/app/landing-page/showreel-works/showreel-works.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fb-showreel-works',
+  templateUrl: './showreel-works.component.html',
+  styleUrls: ['./showreel-works.component.css']
+})
+export class ShowreelWorksComponent {
+
+}
