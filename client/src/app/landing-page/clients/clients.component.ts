596f5a037774fc6aebf41d4094e5be80e2339eed
andreaDiClaudio <andreadicla@gmail.com>
2023-11-22 17:55:38 +0100

    created contact section

--- client/src/app/landing-page/clients/clients.component.ts
+++ client/src/app/landing-page/clients/clients.component.ts
@@ -7,22 +7,24 @@ import { Component } from '@angular/core';
 })
 export class ClientsComponent {
 
+  pathToImage: string = "assets/images/landingPage/clientsLogo/"
+
   clientsLogo: string[] = [
-    "assets/images/clientsLogo/angus_logo.png",
-    "assets/images/clientsLogo/DFDS_logo.png",
-    "assets/images/clientsLogo/golden_goose_logo.png",
-    "assets/images/clientsLogo/helpii_logo.png",
-    "assets/images/clientsLogo/ironman-vector-logo.png",
-    "assets/images/clientsLogo/mg_logo.png",
-    "assets/images/clientsLogo/nhcollection_logo.png",
-    "assets/images/clientsLogo/Novo_Nordisk_-_Logo.png",
-    "assets/images/clientsLogo/nuna_logo.png",
-    "assets/images/clientsLogo/our_shift_logo.png",
-    "assets/images/clientsLogo/tiptapp_logo.png",
-    "assets/images/clientsLogo/UNICEF_logo.png",
-    "assets/images/clientsLogo/World_Chess_Championship_Logo.png",
-    "assets/images/clientsLogo/wooshii_logo.png",
-    "assets/images/clientsLogo/gods_logo.png"
+    this.pathToImage + "angus_logo.png",
+    this.pathToImage + "DFDS_logo.png",
+    this.pathToImage + "golden_goose_logo.png",
+    this.pathToImage + "helpii_logo.png",
+    this.pathToImage + "ironman-vector-logo.png",
+    this.pathToImage + "mg_logo.png",
+    this.pathToImage + "nhcollection_logo.png",
+    this.pathToImage + "Novo_Nordisk_-_Logo.png",
+    this.pathToImage + "nuna_logo.png",
+    this.pathToImage + "our_shift_logo.png",
+    this.pathToImage + "tiptapp_logo.png",
+    this.pathToImage + "UNICEF_logo.png",
+    this.pathToImage + "World_Chess_Championship_Logo.png",
+    this.pathToImage + "wooshii_logo.png",
+    this.pathToImage + "gods_logo.png"
   ]
 
 }
--- client/src/app/landing-page/landing-page.module.ts
+++ client/src/app/landing-page/landing-page.module.ts
@@ -6,18 +6,22 @@ import { LandingPageComponent } from './landing-page.component';
 import {NavbarModule} from "../navbar/navbar.module";
 import { ShowreelWorksComponent } from './showreel-works/showreel-works.component';
 import { ClientsComponent } from './clients/clients.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import {FooterModule} from "../footer/footer.module";
 
 
 @NgModule({
   declarations: [
     LandingPageComponent,
     ShowreelWorksComponent,
-    ClientsComponent
+    ClientsComponent,
+    ContactsComponent
   ],
-    imports: [
-        CommonModule,
-        LandingPageRoutingModule,
-        NavbarModule
-    ]
+  imports: [
+    CommonModule,
+    LandingPageRoutingModule,
+    NavbarModule,
+    FooterModule
+  ]
 })
 export class LandingPageModule { }
