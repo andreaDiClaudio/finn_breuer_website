b01b41a2375fd4b627e30202386800662b74d0e3
andreaDiClaudio <andreadicla@gmail.com>
2023-11-22 17:10:08 +0100

    created clients section

--- client/src/app/landing-page/clients/clients.component.ts
+++ client/src/app/landing-page/clients/clients.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class ClientsComponent {
 
+  clientsLogo: string[] = [
+    "assets/images/clientsLogo/angus_logo.png",
+    "assets/images/clientsLogo/DFDS_logo.png",
+    "assets/images/clientsLogo/golden_goose_logo.png",
+    "assets/images/clientsLogo/helpii_logo.png",
+    "assets/images/clientsLogo/ironman-vector-logo.png",
+    "assets/images/clientsLogo/mg_logo.png",
+    "assets/images/clientsLogo/nhcollection_logo.png",
+    "assets/images/clientsLogo/Novo_Nordisk_-_Logo.png",
+    "assets/images/clientsLogo/nuna_logo.png",
+    "assets/images/clientsLogo/our_shift_logo.png",
+    "assets/images/clientsLogo/tiptapp_logo.png",
+    "assets/images/clientsLogo/UNICEF_logo.png",
+    "assets/images/clientsLogo/World_Chess_Championship_Logo.png",
+    "assets/images/clientsLogo/wooshii_logo.png",
+    "assets/images/clientsLogo/gods_logo.png"
+  ]
+
 }
