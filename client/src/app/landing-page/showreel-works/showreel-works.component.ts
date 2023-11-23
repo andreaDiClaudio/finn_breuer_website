4ee17cfd81d76006c880ddfc6598f0600ac5cb08
andreaDiClaudio <andreadicla@gmail.com>
2023-11-23 18:08:15 +0100

    minor improvement

--- client/src/app/landing-page/showreel-works/showreel-works.component.ts
+++ client/src/app/landing-page/showreel-works/showreel-works.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class ShowreelWorksComponent {
 
+  showReelHeight: string = "";
+  worksHeight: string = "";
+
+  constructor() {
+    if (window.innerHeight > 825) {
+      this.showReelHeight = "h-[58vh] min-h-[540px]"
+      this.worksHeight= "h-[40.5vh] min-h-[375px]"
+    } else {
+      this.showReelHeight = "h-[58vh] min-h-[477px] "
+      this.worksHeight= "h-[40vh] min-h-[330px]"
+    }
+  }
+
 }
