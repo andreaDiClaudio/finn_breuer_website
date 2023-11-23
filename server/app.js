6a1f0c75149965870f99714a2dc8f2f2d0cdf4e8
andreaDiClaudio <andreadicla@gmail.com>
2023-11-23 11:10:25 +0100

    created server

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
+//needed to read .env file
+dotenv.config();
 
 app.use(cors({
     origin: 'http://localhost:4200' //replace with your domain or specific front-end origin
 }));
 
-//needed to read .env file
-dotenv.config();
-
 const PORT = process.env.PORT
 app.listen((PORT), () => {
     console.log("Server is running on port:", PORT)
 })
+
